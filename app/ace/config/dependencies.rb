7d927f3c1480aba542a765dc8352c1545e772744
Ryan Stout <ryanstout@gmail.com>
2014-09-23 19:01:58 -0600

    WIP

--- app/ace/config/dependencies.rb
+++ app/ace/config/dependencies.rb
@@ -1,1 +1,2 @@
 # Component dependencies
+javascript_file '//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ace.js'
--- app/ace/controllers/editor_controller.rb
+++ app/ace/controllers/editor_controller.rb
@@ -1,4 +1,21 @@
 module Ace
   class EditorController < ModelController
+    attr_accessor :section
+    def index
+    end
+
+    def dom_ready
+      puts "Index: #{section.container_node}"
+
+      node = section.container_node
+
+      %x{
+        var editor = ace.edit('aceEditor');
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/javascript");
+      }
+
+
+    end
   end
 end
